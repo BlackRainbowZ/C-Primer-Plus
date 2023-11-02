991fc2b56ce1b5cb0a8c3e346aede2251829d37c
lcq22 <3601611747@qq.com>
2023-11-02 15:09:19 +0800

    Chapter I

---
+++ CPP1/cpp1.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//程序清单1.1 C源代码示例
+//#include <stdio.h>
+//int main(void)
+//{
+//	int dogs;
+//
+//	printf("How many dogs do you have?\n");
+//	scanf("%d", &dogs);
+//	printf("So you have %d dog(s)!", dogs);
+//
+//	return 0;
+//}
+
+//程序清单1.2 C程序
+//#include <stdio.h>
+//int main(void)
+//{
+//	printf("Concrete contains grevel and cement.\n");
+//
+//	return 0;
+//}
+
+//1.8.2.1
+#include <stdio.h>
+int main(void)
+{
+	printf("A.c is used to end a C program filename.\n");
+
+	return 0;
+}
